5321d6ca2aa47aac4623eb85fae46a5f63641a63
hyemory <cjstksla73@gmail.com>
2023-06-13 22:03:13 +0900

    feat: MeasuredData Model 생성

---
+++ GyroData/MeasuredData.swift
@@ -0,0 +1,36 @@
+//
+//  MeasuredData.swift
+//  GyroData
+//
+//  Created by Hyejeong Jeong on 2023/06/13.
+//
+
+import Foundation
+
+struct MeasuredData {
+    let id: UUID
+    let date: Date
+    let sensor: Sensor
+    let time: Double
+    let sensorData: SensorData
+}
+
+enum Sensor {
+    case accelerometer
+    case gyroscope
+    
+    var title: String {
+        switch self {
+        case .accelerometer:
+            return "Accelerometer"
+        case .gyroscope:
+            return "Gyro"
+        }
+    }
+}
+
+struct SensorData {
+    let x: [Double]
+    let y: [Double]
+    let z: [Double]
+}
